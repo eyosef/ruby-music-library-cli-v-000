5e4882f49e902fea6e8994144de1201e9538e7fc
Eden Mesfun <emesfun@gmail.com>
2018-02-23 20:43:43 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -80,7 +80,8 @@ class MusicLibraryController
 
     input = user_input.to_i - 1
     list = []
-    
+    if 6 < input > 0
+
     Song.all.sort {|a, b| a.name <=> b.name }.each_with_index { |song, index|
        list << "Playing #{song.name} by #{song.artist.name}"}
        puts list[input] unless list[input] == nil
