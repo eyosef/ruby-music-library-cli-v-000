3e8d0c9fad51c2e939117bc6482cbfbc823096f9
Eden Mesfun <emesfun@gmail.com>
2018-02-23 21:26:58 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -34,9 +34,9 @@ class MusicLibraryController
         when "list genres"
           list_genres
         when "list artist"
-          list_songs_by_artist 
+          list_songs_by_artist
         when "list genre"
-          list_songs_by_genre 
+          list_songs_by_genre
         when "play song"
           play_song
     end #case
