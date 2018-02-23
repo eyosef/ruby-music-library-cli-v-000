35e2809142167f6b1d9d9d9395c8458f04be3cf5
Eden Mesfun <emesfun@gmail.com>
2018-02-23 21:18:57 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -24,7 +24,7 @@ class MusicLibraryController
     case invoke
         when user_input = "list songs"
           list_songs
-        elsif user_input = "list artists"
+        when user_input = "list artists"
           list_artists
         elsif user_input = "list genres"
           list_genres
