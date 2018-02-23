4be3751d713997b96d500099c4b495387f2834db
Eden Mesfun <emesfun@gmail.com>
2018-02-23 21:24:38 +0000

    automatically backed up by learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -20,7 +20,7 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
 
-    
+    #user_input = nil
     until user_input == "exit"
       user_input = gets.chomp
     end
